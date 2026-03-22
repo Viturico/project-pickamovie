eeef24197977ae2b85244d6eb530cb9b3882d78d
Viturico <albyrg@gmail.com>
2026-03-22 20:27:36 +0000

    SwiperJS en los Sliders + Estilos | Cards de prueba

--- src/main.js
+++ src/main.js
@@ -1,3 +1,25 @@
-const prueba = "prueba";
+const exploraTitle = document.getElementById("explora-title");
+let containerPosX = "";
 
-let pruebas = "prueba2";
+const swipers = document.querySelectorAll(".swiper");
+
+swipers.forEach((swiperEl) => {
+  let containerPosX = exploraTitle.getBoundingClientRect().left - 16;
+
+  new Swiper(swiperEl, {
+    slidesPerView: "auto",
+    spaceBetween: 30,
+    slidesOffsetBefore: containerPosX,
+    navigation: {
+      nextEl: swiperEl.querySelector(".swiper-button-next"),
+      prevEl: swiperEl.querySelector(".swiper-button-prev"),
+      addIcons: false,
+    },
+    pagination: {
+      el: swiperEl.querySelector(".swiper-pagination"),
+      clickable: true,
+    },
+  });
+});
+
+console.log(containerPosX);
